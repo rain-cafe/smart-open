f6da978b4fb1fe10c724aed010248f524107fd51
Cecilia Sanare <ceci@sanare.dev>
2024-02-22 19:15:38 -0600

    fix: try to make program check more resilient

--- src/utils/command.rs
+++ src/utils/command.rs
@@ -1,22 +1,10 @@
-use std::{env, fs, path::Path};
-
-pub fn get_path() -> Vec<String> {
-    let path = env::var("PATH").unwrap_or_default();
-
-    let split = if cfg!(target_family = "windows") { path.split(";") } else { path.split(":") };
-
-    split.map(|x| x.to_string()).collect()
-}
+use std::process::{Command, Stdio};
 
 pub fn is_program_in_path(program: &str) -> bool {
-    for p in get_path() {
-        let buf = Path::new(&p).join(program);
-        let p_str = buf.to_str().expect("Failed to join");
-        if fs::metadata(p_str).is_ok() {
-            return true;
-        }
-    }
-    false
+    return match Command::new(program).stdout(Stdio::null()).stderr(Stdio::null()).spawn() {
+        Ok(_) => true,
+        Err(_) => false, 
+    };
 }
 
 pub fn is_program_not_in_path(program: &str) -> bool {
@@ -25,15 +13,11 @@ pub fn is_program_not_in_path(program: &str) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::command::{get_path, is_program_in_path, is_program_not_in_path};
-
-    #[test]
-    fn get_path_should_parse_the_path() {
-        assert!(get_path().len() > 0);
-    }
+    use crate::utils::command::{is_program_in_path, is_program_not_in_path};
 
     #[test]
     fn is_program_in_path_should_return_true_if_program_exists() {
+        assert_eq!(is_program_in_path("git"), true);
         assert_eq!(is_program_in_path("cargo"), true);
     }
 
@@ -44,6 +28,7 @@ mod tests {
 
     #[test]
     fn is_program_not_in_path_should_return_false_if_program_exists() {
+        assert_eq!(is_program_not_in_path("git"), false);
         assert_eq!(is_program_not_in_path("cargo"), false);
     }
 
